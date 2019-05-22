0a928b7d8a827e763493c0bb65c2fce214d55067
Andnrew Cheon <cheonandrew113@gmail.com>
2019-05-22 15:34:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,10 +19,8 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string === string.toLowerCase)
   return "I can't hear you!"
-}
- 
- function sayHiToGrandma(string){ 
-  if (string === string.toUpperCase)
+  else if (string === string.toUpperCase)
   return "YES INDEED!"
+  else 
 }
 
